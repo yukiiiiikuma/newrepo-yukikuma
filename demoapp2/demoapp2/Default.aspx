5542145f1f7219625e5711a85c1914763ed58dff
yukiko.hiramatsu <yukiko.hiramatsu@bigtreetc.com>
2021-08-22 22:17:13 +0900

    テスト

---
+++ demoapp2/demoapp2/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Default.aspx.vb" Inherits="demoapp2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            This is a simple application.
+        </div>
+    </form>
+</body>
+</html>
---
+++ demoapp2/demoapp2/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は、以下の属性セットによって 
+' 制御されます。アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更します。
+
+' アセンブリの属性値を確認します
+<Assembly: AssemblyTitle("demoapp2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("demoapp2")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("359dd942-33df-4eb8-9d94-f6cd46661d15")>
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
